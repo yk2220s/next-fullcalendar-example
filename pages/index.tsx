83e779321303f60f633f598d3d8fe40de74dc731
yk2220s <yk2220s@gmail.com>
2020-12-06 20:41:58 +0900

    add fullcalendar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,14 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import Layout from "../components/Layout";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
+    <FullCalendar
+      plugins={[dayGridPlugin]}
+      initialEvents={[{ title: "initial event", start: new Date() }]}
+    />
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
